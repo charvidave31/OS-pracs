8862af8b1bbc5c0ee74cc8c5e6159c1f17db95e6
charvidave31 <75248271+charvidave31@users.noreply.github.com>
2020-12-02 11:26:49 +0530

    Add files via upload
    I am yet to finish the code, I will complete it entirely and reupload both files before next week.

---
+++ untitled1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 09:45:05 2020
+
+@author: Charvi
+"""
+
+
+from scheduler import fcfs
+
+processes=[1,2,3]
+n=len(processes)
+bt=[10,5,8]
+fcfs(processes,n,bt)
